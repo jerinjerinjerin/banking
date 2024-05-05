235c7708eaa3d919cc1c607042e2d025aa349e0b
jerinrajjsss <jerinr960@gmail.com>
2024-05-05 06:34:06 +0530

    slet and right sidebar layout and credit cart design complect in ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
 
-  const loggedIn = {firstName:'jerin'}
+  const loggedIn = {firstName:'jerin', lastName:'Raj', email:'jerin@gmail.com'}
 
   return (
     <section className='home'>
@@ -23,7 +24,13 @@ const Home = () => {
              totalCurrentBalance={1250.35}
            />
         </header>
+        Recent Transactions
       </div>
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.50}, {}]}
+      />
     </section>
   )
 }
