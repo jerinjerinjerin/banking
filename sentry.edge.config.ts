629fb17371075c951c8f263794a7b7ab30a45ba1
jerinrajjsss <jerinr960@gmail.com>
2024-05-08 22:00:40 +0530

    implement plaid link and Dwolla

---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://6982a25354da9fc7ad4c3175cfbb339d@o4507210215260160.ingest.us.sentry.io/4507210219847680",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
